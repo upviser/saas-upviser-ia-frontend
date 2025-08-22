ffc285e345196fc86e3639b498a7f9616da40180
upvisor <admin@upvisor.cl>
2025-08-22 00:51:50 -0400

    actualization general

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -4,8 +4,14 @@ import { ICartProduct } from '../../interfaces'
 import CartContext from './CartContext'
 import { useSession } from 'next-auth/react'
 import axios from 'axios'
+import { useSearchParams } from 'next/navigation'
 
 const CartProvider: React.FC<PropsWithChildren> = ({ children }) => {
+
+  const searchParams = useSearchParams();
+  const number = searchParams.get('number');
+  const messengerId = searchParams.get('messengerId');
+  const instagramId = searchParams.get('instagramId');
 
   const [cart, setCart] = useState<ICartProduct[]>()
   const [cartView, setCartView] = useState('hidden')
@@ -16,15 +22,28 @@ const CartProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const user = session?.user as { firstName: string, lastName: string, email: string, _id: string, cart: ICartProduct[] }
 
   const getCart = async () => {
-    if (status === 'authenticated') {
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/account/${user._id}`)
-      if (response.data.cart) {
-        localStorage.setItem('cart', JSON.stringify(response.data.cart))
-        setCart(response.data.cart)
+    if (number || messengerId || instagramId) {
+      if (number) {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/cart/${number}`)
+        setCart(res.data.cart)
+      } else if (messengerId) {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/cart/${messengerId}`)
+        setCart(res.data.cart)
+      } else if (instagramId) {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/cart/${instagramId}`)
+        setCart(res.data.cart)
       }
     } else {
-      if (localStorage.getItem('cart')) {
-        setCart(JSON.parse(localStorage.getItem('cart')!))
+      if (status === 'authenticated') {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/account/${user._id}`)
+        if (response.data.cart) {
+          localStorage.setItem('cart', JSON.stringify(response.data.cart))
+          setCart(response.data.cart)
+        }
+      } else {
+        if (localStorage.getItem('cart')) {
+          setCart(JSON.parse(localStorage.getItem('cart')!))
+        }
       }
     }
   }
