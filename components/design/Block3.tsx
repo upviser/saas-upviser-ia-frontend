0514ccbf10a6c0c11a728a87bc55cbf8d9ce7b40
upvisor <admin@upvisor.cl>
2025-10-21 13:09:07 -0300

    actualization general

--- components/design/Block3.tsx
+++ components/design/Block3.tsx
@@ -214,7 +214,7 @@ export const Block3 = ({ content, index, calls, forms, design, payment, style, s
                               setPopup({ ...popup, view: 'flex', opacity: 'opacity-1' })
                             }, 10);
                           }} config='mx-auto lg:m-0' style={style}>{content.info.button}</Button>
-                          : (content.info.buttonLink && content.info.buttonLink !== '') || (content.info.button && content.info.button !== '')
+                          : (content.info.buttonLink && content.info.buttonLink !== '') && (content.info.button && content.info.button !== '')
                             ? content.info.buttonLink === 'Abrir Whatsapp'
                               ? <button className={`w-fit flex text-center py-2 px-6 font-medium`} style={{ backgroundColor: style.primary, color: style.button, borderRadius: style.form === 'Redondeadas' ? `${style.borderButton}px` : '' }} onClick={() => window.open(`https://wa.me/+56${storeData?.phone}`)}>{content.info.button}</button>
                               : content.info.url && content.info.url !== ''
@@ -232,7 +232,7 @@ export const Block3 = ({ content, index, calls, forms, design, payment, style, s
                               setPopup({ ...popup, view: 'flex', opacity: 'opacity-1' })
                             }, 10);
                           }} config='mx-auto' style={style} content={content}>{content.info.button2}</ButtonSecondary>
-                          : (content.info.buttonLink2 && content.info.buttonLink2 !== '') || (content.info.button2 && content.info.button2 !== '')
+                          : (content.info.buttonLink2 && content.info.buttonLink2 !== '') && (content.info.button2 && content.info.button2 !== '')
                             ? content.info.url2 && content.info.url2 !== ''
                               ? <Link href={`${content.info.url2}`} target='_blank' className='mx-auto'><ButtonSecondary style={style} content={content}>{content.info.button2}</ButtonSecondary></Link>
                               : <Link href={`${content.info.buttonLink2}`} className='mx-auto'><ButtonSecondary style={style} content={content}>{content.info.button2}</ButtonSecondary></Link>
